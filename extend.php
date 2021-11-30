3ba48cb1208f4ef014fb657c1afcbb7123732132
block-cat <iuliancernei@gmail.com>
2021-11-30 17:30:36 +0200

    allowGuest vote articles

--- extend.php
+++ extend.php
@@ -17,9 +17,9 @@ return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/resources/less/forum.less'),
-    // (new Extend\Frontend('admin'))
-    //     ->js(__DIR__.'/js/dist/admin.js')
-    //     ->css(__DIR__.'/resources/less/admin.less'),
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__.'/js/dist/admin.js')
+        ->css(__DIR__.'/resources/less/admin.less'),
     // new Extend\Locales(__DIR__ . '/resources/locale'),
     (new Extend\ServiceProvider())
         ->register(Providers\GamificationProvider::class),
--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,3 +1,16 @@
+import { extend } from 'flarum/common/extend';
+import PermissionGrid from 'flarum/admin/components/PermissionGrid';
+
+function allowGuest(items, key) {
+  if (!items.has(key)) {
+      return;
+  }
+
+  items.get(key).allowGuest = true;
+}
+
 app.initializers.add('block-cat/guest-gamification', () => {
-  console.log('[block-cat/guest-gamification] Hello, admin!');
+  extend(PermissionGrid.prototype, 'replyItems', items => {
+    allowGuest(items, 'discussion.votePosts');
+  });
 });
